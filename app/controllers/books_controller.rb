94b1bd1a4a621d59b84b82875d04906007531be3
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-06-28 14:37:07 +0000

    [Update] 更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,30 @@ class BooksController < ApplicationController
   def top
   end
 
-  def new
+  def index
+    @Book = Book.new
+    
   end
 
-  def index
+  def create
+    @Books = Book.new(book_params)
+    if @Book.save
+      falsh[:notice] = "Book was successfully created."
+      redirect_to book_path(@Book.id)
+    else
+      render :index
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
